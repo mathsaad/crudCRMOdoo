40eb8f1faa0564ffacbcedee6de119ca10f69b0c
Matheus <matheus.adriano@digitro.com.br>
2019-08-08 10:31:18 -0300

    adicionado search por id, email, update, delete

--- app/controllers/crm_lead_controller.py
+++ app/controllers/crm_lead_controller.py
@@ -5,18 +5,34 @@ from app.services import crm_lead
 
 crmLead = crm_lead.CrmLead()
 
-@app.route('/search')
+@app.route('/search', methods=['GET'])
 def search():
     result = crmLead.searchAndRead()
     return jsonify(result), 200
 
+@app.route('/search/<int:idlead>', methods=['GET'])
+def searchById(idlead):
+    result = crmLead.searchById(idlead)
+    return jsonify(result), 200
+
+@app.route('/search/<string:mail>', methods=['GET'])
+def searchByMail(mail):
+    result = crmLead.searchByEmail(mail)
+    return jsonify(result), 200
+
 @app.route('/add/lead', methods=['POST'])
 def createLead():
-    lead = request.get_json();
+    lead = request.get_json()
     result = crmLead.createLead(lead)
     return jsonify(result), 201
-    
+        
+@app.route('/update/<int:idlead>', methods=['PUT'])
+def updateLead(idlead):
+    lead = request.get_json()
+    result = crmLead.updateLead(idlead, lead)
+    return jsonify(result), 200
 
-    # {'name': 'New Lead By API', 'email_from': 'saadrcaa@gmail.com',
-    #                                            'website': False, 'kanban_state': 'green',
-    #                                            'priority': '3', 'write_date': '10-02-2019'}
+@app.route('/delete/<int:idlead>', methods=['DELETE'])
+def deleteLead(idlead):
+    crmLead.deleteById(idlead)
+    return   jsonify({'message': 'Lead deletado com sucesso!'}), 200
--- app/services/crm_lead.py
+++ app/services/crm_lead.py
@@ -40,19 +40,21 @@ class CrmLead():
                                                   'kanban_state', 'create_date', 'write_date', 'priority'], 'limit': 20})
 
     def createLead(self, lead):
-        self.idLead = self.object.execute_kw(self.od.db, self.od.uid, self.od.password, 'crm.lead', 'create',
-                                             [lead])
+        self.idLead = self.object.execute_kw(self.od.db, self.od.uid, self.od.password, 'crm.lead', 'create', [lead])
         return self.idLead
 
-    def updateLead(self, idlead):
-        return self.object.execute_kw(self.od.db, self.od.uid, self.od.password, 'crm.lead', 'write',
-                                      [[idlead], {'name': 'Newer Lead By API'}])
+    def updateLead(self, idlead, lead):
+        self.object.execute_kw(self.od.db, self.od.uid, self.od.password, 'crm.lead', 'write',
+                                      [idlead, lead])
+        return self.object.execute_kw(self.od.db, self.od.uid, self.od.password, 'crm.lead', 'read', [idlead])
 
     def searchById(self, idlead):
-        return self.object.execute_kw(self.od.db, self.od.uid, self.od.password, 'crm.lead', 'name_get', [[idlead]])
+        return self.object.execute_kw(self.od.db, self.od.uid, self.od.password, 'crm.lead', 'read', [idlead])
+
+    def searchByEmail(self, mail):
+        return self.object.execute_kw(self.od.db, self.od.uid, self.od.password, 'crm.lead', 'search_read', [[['email_from', '=', mail]]])
 
     def deleteById(self, idlead):
-        self.object.execute_kw(self.od.db, self.od.uid, self.od.password, 'crm.lead', 'unlink', [[idlead]])
-        delUser = self.object.execute_kw(self.od.db, self.od.uid, self.od.password, 'crm.lead', 'search',
-                                         [[['id', '=', idlead]]])
+        self.object.execute_kw(self.od.db, self.od.uid, self.od.password, 'crm.lead', 'unlink', [idlead])
+        delUser = self.object.execute_kw(self.od.db, self.od.uid, self.od.password, 'crm.lead', 'search', [[['id', '=', idlead]]])
         return delUser
